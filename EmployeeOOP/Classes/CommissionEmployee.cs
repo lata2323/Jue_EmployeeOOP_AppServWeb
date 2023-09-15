0d3412b3fa7140047a950c92a9f11f543344a689
lata2323 <ltapiasa@gmail.com>
2023-09-15 00:08:13 -0500

    Class CommissionEmployee created

---
+++ EmployeeOOP/Classes/CommissionEmployee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP.Classes
+{
+    public class CommissionEmployee : Employee
+    {
+        #region Properties
+
+        public decimal CommissionPercentaje { get; set; }
+        public decimal Sales { get; set; }
+        public decimal Salary { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public CommissionEmployee()
+        {
+            
+        }
+
+        public override decimal GetValueToPay()
+        {
+            return CommissionPercentaje / 100 * Sales;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" + //base hace referencia a la clase padre, al método ToString
+                $"Salario Devengado: {GetValueToPay():C2}\n\t";
+        }
+
+        #endregion
+    }
+}
--- EmployeeOOP/Program.cs
+++ EmployeeOOP/Program.cs
@@ -5,7 +5,7 @@ try
 {
     int day, month, year;
     string firstName, lastName;
-    decimal salary;
+    decimal salary, commissionPercentaje, sales;
 
     Console.WriteLine("OOP APPLICATION");
     Console.WriteLine("---------------");
@@ -24,7 +24,9 @@ try
     Console.WriteLine("\n");
     Console.WriteLine(dateObject.ToString());
 
-    Console.Write("Ingresar nombre completo: ");
+    #region SalaryEmployee
+    //Código para SalaryEmployee
+    /*Console.Write("Ingresar nombre completo: ");
     firstName = Console.ReadLine();
 
     Console.Write("Ingresar apellidos: ");
@@ -44,8 +46,38 @@ try
         Salary = salary,
     };
 
-    Console.WriteLine(salaryEmployee);
+    Console.WriteLine(salaryEmployee);*/
+    #endregion
 
+    #region CommissionEmployee
+    //Código para CommissionEmployee
+    Console.Write("Ingresar nombre completo: ");
+    firstName = Console.ReadLine();
+
+    Console.Write("Ingresar apellidos: ");
+    lastName = Console.ReadLine();
+
+    Console.Write("Ingresar el porcentaje de la comisión: ");
+    commissionPercentaje = Decimal.Parse(Console.ReadLine());
+
+    Console.Write("Ingresar el valor de las ventas: ");
+    sales = Decimal.Parse(Console.ReadLine());
+
+    CommissionEmployee commissionEmployee = new CommissionEmployee()
+    {
+        Id = 123456,
+        FirstName = firstName,
+        LastName = lastName,
+        BirthDate = dateObject,
+        HiringDate = dateObject,
+        IsActive = true,
+        CommissionPercentaje = commissionPercentaje,
+        Sales = sales,
+    };
+
+    Console.WriteLine(commissionEmployee);
+
+    #endregion
 }
 catch (Exception ex)
 {
